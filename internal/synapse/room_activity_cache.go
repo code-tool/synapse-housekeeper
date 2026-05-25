00e270b82af34ca3b93f8ebb22d3d35e56e1cdea
asp24 <488588+asp24@users.noreply.github.com>
2026-05-25 14:04:01 +0200

    refactor(synapse): extract RoomActivityCacheNull to a separate file

--- internal/synapse/room_activity_cache.go
+++ internal/synapse/room_activity_cache.go
@@ -2,8 +2,6 @@ package synapse
 
 import (
 	"context"
-	"io"
-	"strings"
 	"time"
 
 	"maunium.net/go/mautrix/id"
@@ -19,33 +17,4 @@ type RoomActivityCacheEntry struct {
 type RoomActivityCache interface {
 	RoomActivity(ctx context.Context, roomID id.RoomID) (*RoomActivityCacheEntry, error)
 	StoreRoomActivity(ctx context.Context, entry RoomActivityCacheEntry) error
-}
-
-type RoomActivityCacheNull struct{}
-
-func NewRoomActivityCache(ctx context.Context, dsn string) (RoomActivityCache, io.Closer, error) {
-	if strings.TrimSpace(dsn) == "" {
-		return RoomActivityCacheNull{}, noopCloser{}, nil
-	}
-
-	cache, err := NewRoomActivityCachePostgres(ctx, dsn)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return cache, cache, nil
-}
-
-func (RoomActivityCacheNull) RoomActivity(ctx context.Context, roomID id.RoomID) (*RoomActivityCacheEntry, error) {
-	return nil, nil
-}
-
-func (RoomActivityCacheNull) StoreRoomActivity(ctx context.Context, entry RoomActivityCacheEntry) error {
-	return nil
-}
-
-type noopCloser struct{}
-
-func (noopCloser) Close() error {
-	return nil
 }
---
+++ internal/synapse/room_activity_cache_null.go
@@ -0,0 +1,38 @@
+package synapse
+
+import (
+	"context"
+	"io"
+	"strings"
+
+	"maunium.net/go/mautrix/id"
+)
+
+type noopCloser struct{}
+
+func (noopCloser) Close() error {
+	return nil
+}
+
+type RoomActivityCacheNull struct{}
+
+func NewRoomActivityCache(ctx context.Context, dsn string) (RoomActivityCache, io.Closer, error) {
+	if strings.TrimSpace(dsn) == "" {
+		return RoomActivityCacheNull{}, noopCloser{}, nil
+	}
+
+	cache, err := NewRoomActivityCachePostgres(ctx, dsn)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return cache, cache, nil
+}
+
+func (RoomActivityCacheNull) RoomActivity(ctx context.Context, roomID id.RoomID) (*RoomActivityCacheEntry, error) {
+	return nil, nil
+}
+
+func (RoomActivityCacheNull) StoreRoomActivity(ctx context.Context, entry RoomActivityCacheEntry) error {
+	return nil
+}
